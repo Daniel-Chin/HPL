5f0d0feaeef817558a45b3b7da74103c20c8797e
Daniel Chin <daniel.chin@nyu.edu>
2021-10-24 19:22:40 -0400

    ignore -> unit

--- miniOO/miniOO.ml
+++ miniOO/miniOO.ml
@@ -5,7 +5,7 @@ try
   let lexbuf = Lexing.from_channel stdin in
   while true do
     try
-      MENHIR.prog LEX.token lexbuf
+      ignore (MENHIR.prog LEX.token lexbuf)
     with Parse_error ->
       (
         (print_string "Syntax error ..." ; print_newline ()) ;
